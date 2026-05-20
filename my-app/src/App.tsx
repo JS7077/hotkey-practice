5b0814d20d741cda7fc4b16a3b6166785ac7f387
JS7077 <139284744+JS7077@users.noreply.github.com>
2026-05-20 09:33:16 -0700

    got keys registering

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,18 +1,35 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './App.css'
 import { Hotbar } from './Hotbar'
-import { KEYS } from './Types'
+import { KEYS, type KEY } from './Types'
 
 function App() {
   const hotbar = [KEYS.ONE,KEYS.TWO,KEYS.THREE,KEYS.FOUR,KEYS.FIVE,KEYS.SIX,KEYS.SEVEN,KEYS.EIGHT,KEYS.NINE]
   const keybinds = useMemo(()=>hotbar,hotbar)
+  const [keyPressed, setKeyPressed] = useState('')
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      setKeyPressed((event.key as string).toUpperCase())
+      switch (keyPressed) {
+          case 'SHIFT': setKeyPressed(event.location === 1 ? KEYS.LSHIFT : KEYS.RSHIFT); break
+          case 'ALT': setKeyPressed(event.location === 1 ? KEYS.LALT : KEYS.RALT); break
+          case 'META': setKeyPressed(event.location === 1 ? KEYS.LMETA : KEYS.RMETA); break
+      }
+      console.log(keyPressed)
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    // Cleanup function
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
   return (
     <div style={{
       justifyContent: 'center',
       alignItems: 'center',
       height: '100svh'
     }}>
-      <Hotbar hotbar={keybinds} />
+      <Hotbar hotbar={keybinds} keyPressed={keyPressed as KEY} />
     </div>
   )
 }
--- my-app/src/Hotbar.tsx
+++ my-app/src/Hotbar.tsx
@@ -1,27 +1,13 @@
-import { useEffect, type JSX } from "react";
+import { type JSX } from "react";
 import { KEYS, type KEY } from "./Types"
 
-export function Hotbar({hotbar}:{hotbar:KEY[]}) {
+export function Hotbar({hotbar, keyPressed}:{hotbar:KEY[], keyPressed: KEY}) {
     const slots = hotbar.map(x => <Slot key={x} keybind={x} />)
+    // slots[0] = <Selector slot={<Slot keybind={KEYS.A} />} />
 
-    useEffect(() => {
-        const handleKeyDown = (event) => {
-            let keyPressed = (event.key as string).toUpperCase()
-            switch (keyPressed) {
-                case 'SHIFT': keyPressed = event.location === 1 ? KEYS.LSHIFT : KEYS.RSHIFT; break
-                case 'ALT': keyPressed = event.location === 1 ? KEYS.LALT : KEYS.RALT; break
-                case 'META': keyPressed = event.location === 1 ? KEYS.LMETA : KEYS.RMETA; break
-            }
-            let i = hotbar.indexOf(keyPressed as KEY)
-            if(i > 8 || i < 0) i = 0
-            slots[i] = <Selector slot={slots[i]} />
-        };
-
-        window.addEventListener('keydown', handleKeyDown);
-        
-        // Cleanup function
-        return () => window.removeEventListener('keydown', handleKeyDown);
-    }, []);
+    let i = hotbar.indexOf(keyPressed as KEY)
+    if(i > 8 || i < 0) i = 0
+    slots[i] = <Selector slot={slots[i]} />
 
     return (
         <div style={{
